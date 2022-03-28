a33e7f8c943f0ef64a8f6da7aa263ac7c75d996d
rajaaghanem <rajaaghanem@gmail.com>
2022-03-28 16:25:27 +0300

    add post axios request using jira API

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,15 +1,16 @@
-import "regenerator-runtime/runtime.js";
+import 'regenerator-runtime/runtime.js';
 import axios from 'axios';
 import fetchAdapter from '@vespaiach/axios-fetch-adapter';
 console.log('This is the background page.');
-console.log('Put the background scripts here.');
 
 // chrome.actiont.onClicked.addListener((tab) => {
 //   chrome.scripting.executeScript(tab.id, { file: 'contentScript.bundle.js' });
 // });
+
+
 let currentUrl, currentTabId;
-chrome.tabs.onUpdated.addListener(function (tabId, {url}, tab) {
-  if(currentUrl !== url || currentTabId !== tabId){
+chrome.tabs.onUpdated.addListener(function (tabId, { url }, tab) {
+  if (currentUrl !== url || currentTabId !== tabId) {
     chrome.scripting.executeScript({
       files: ['contentScript.bundle.js'],
       target: { tabId },
@@ -21,23 +22,35 @@ chrome.tabs.onUpdated.addListener(function (tabId, {url}, tab) {
 
 
 setTimeout(async () => {
+  const obj = {
+    fields: {
+      project: {
+        key: 'ECL',
+      },
+      summary: 'test created with axios',
+
+      issuetype: {
+        name: 'Bug',
+      },
+      // assignee: 'rajaag',
+    },
+  };
+
   try {
-    console.log("in settimeout");
-    const res = await axios.get(`https://pbo.wix.com/fire-console/serverSign?appDefId=${process.env.APP_ID}`,
-    {
-      adapter: fetchAdapter,
-    });
+    console.log('in settimeout');
+    const res = await axios.post(
+      `https://jira.wixpress.com/rest/api/2/issue`,
+      obj,
+      {
+        withCredentials: true,
+        // headers: {
+        //   // 'Authorization': `Basic ${myToken}`,
+        //   Cookie: cookies,
+        // },
+        adapter: fetchAdapter,
+      }
+    );
     console.log(res);
-    const response = await axios({
-      method: 'get',
-      url: `https://bo.wix.com/jira-gateway-web/api/jira/v3/issues/ECL-4276`,
-      headers: { 'Authorization': res.data.signature, 'Content-Type': 'application/json'},
-      mode: 'cors',
-      adapter: fetchAdapter,
-      
-    });
-    // const data = await response.json();
-    console.log(response);
   } catch (e) {
     const errorResult = { error: e.message, exception: e };
     console.log(e);
